cdbb22e7cce0a385698811ffc256a679f1b54b9d
Al Sweigart <asweigart@gmail.com>
2022-06-23 21:27:01 -0500

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # asciiartjsondb
 A collection of ASCII art, stored in a single JSON file.
+
+**If you want to look at the ascii art, click on the asciiartdb-asciiarteu.txt file.**
+
+# Description of Files in This Repo
+
+*downloadasciiartsites.bat* - A Windows batch file that uses wget to download the ascii art files. These commands can also be run on Linux/Mac machines.
+
+*asciiArtEuExtractArt.py* - Extracts ascii art from the downloaded mirror of asciiart.eu and writes it to asciiartdb-asciiarteu.json.
+
+*asciiArtEuConvertAsciiArtJsonToText.py* - Converts the ascii art in the json file asciiartdb-asciiarteu.json to an easier to read asciiartdb-asciiarteu.txt text file.
+
+*asciiartdb-asciiarteu.json* - The ascii art from asciiart.eu in json format, where it can be parsed by programs.
+
+*asciiartdb-asciiarteu.txt* - The ascii art from asciiart.eu in text format, where it can be easily viewed by humans.
---
+++ downloadasciiartsites.bat
@@ -0,0 +1,10 @@
+wget --mirror --convert-links --html-extension --wait=0.1 -o log https://www.asciiart.eu/
+wget --mirror --convert-links --html-extension --wait=0.1 -o log https://ascii.co.uk/art
+wget --mirror --convert-links --html-extension --wait=0.1 -o log https://asciiart.website/
+wget --mirror --convert-links --html-extension --wait=0.1 -o log https://c.r74n.com/textart
+wget --mirror --convert-links --html-extension --wait=0.1 -o log https://fsymbols.com/text-art/
+wget --mirror --convert-links --html-extension --wait=0.1 -o log http://www.ascii-art.de/ascii/
+wget --mirror --convert-links --html-extension --wait=0.1 -o log http://asciimoji.com/
+wget --mirror --convert-links --html-extension --wait=0.1 -o log http://artscene.textfiles.com/asciiart/
+wget --mirror --convert-links --html-extension --wait=0.1 -o log https://www.redicals.com/ascii-art-facebook.html
+wget --mirror --convert-links --html-extension --wait=0.1 -o log https://textart.sh/
